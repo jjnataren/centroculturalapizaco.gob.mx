6ed050c926bac7acbc2c9bc31365277113a97d0a
rafa <rafa@192.168.1.73>
2018-01-10 22:24:26 -0600

    PDF de ficha de inscripcion

--- backend/views/taller-imp/reporte-inscripcion.php
+++ backend/views/taller-imp/reporte-inscripcion.php
@@ -6,29 +6,60 @@
 
 <div style="width:21cm;height:29.7cm;margin:0;" >
 
-<table style="width: 100%; font-size: 12px; border: 1px solid black; font-family:times new roman; font-style:bold;">
+<table style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
 
 
 		<tr>
-			<td align="left" rowspan="3"><img alt="" src="/img/mc.jpg"></td>
-			<th align="left">Multicode SA de CV</th>
-			<th align="right"  style="background: #d4d6d8;">Folio inscripción</th>			
+    		<th align="left"><img alt="" src="/img/tlaxcalalogo.png" width="200" height="130"></th>
+    		<th align="right"></th>
+    		<th align="right"><img alt="" src="/img/LCC.jpg" width="200" height="100"></th>
 		</tr>
-		<tr>
-			<td align="left"><small><font  color="blue">http://www.multicode.com.mx/</font></small></td>
-			<th align="right" rowspan="2"  style="background: #d4d6d8;"><?= $model->id; ?></th>
+			<tr ><th ></th ></tr >
+		
+		<tr >
+<th ></th >
+			<th align="center" ><H2>"LA LIBERTAD"</H2></th>
+			
+			</tr>
+			
+		<tr align="center">
+		<th ></th >
+			<td align="center">CENTRO CULTURAL DE APIZACO</td>
+			
+			
 						
 		</tr>
-		<tr>
-			<td align="left"><small>Calle de los prados #7, Cd. Neza, Edo. de Mexico, CP 678124</small></td>
+		<tr align="center">
+		<th ></th >
+			<td align="center">Av. 5 de Mayo esq. Hidalgo s/n</td>
 			
+	<tr align="center">
+	<th ></th >
+			<td align="center">Aplizaco tlaxcala C.P 90300 Tel/Fax (01-241)41 8 09 60</td>
+		
 		</tr>
 	
 </table>
 
+<table style="width: 100%; font-size: 12px;  font-family:times new roman; font-style:bold;">
+		<tr align="center">
+			<tH style=" width: 60%;" rowspan="2"><h3>FICHA DE INSCRIPCION</h3></tH>
+		
+			<th align="left"  style="background: #d4d6d8;">Fecha de inscripción</th>
+			<th align="left"  style="background: #d4d6d8;">Folio inscripción</th>
+			
+						
+		</tr>
+		<tr align="center">
+			
+		
+			<td align="center"   style="background: #d4d6d8;"><?= $model->fecha_inscripcion; ?></td>
+			<td align="center"   style="background: #d4d6d8;"><?= $model->id; ?></td>
+		</tr>
+</table>
 <br />
 
-<table  style="width:100%; font-size: 10px; border: 1px solid black; font-family:times new roman;">
+<table  style="width:100%; font-size: 17px; border: 1px solid black; font-family:times new roman;">
 			
 		<tr>
 			<td style=" width: 20%;"></td>
@@ -37,27 +68,29 @@
 			<td style=" width: 20%;"></td>
 		</tr>	
 		
+	
 		<tr>
-			
-			<th align="left" style="background: #d4d6d8;"  >Cliente</th>
-			<td align="left" >$model->clave_proveedor;?></td>
-			
-			<td align="left" colspan="2" >isset( $model->claveProveedor)?$model->claveProveedor->nombre : 'Sin cliente' ;?></td>
-					
-		</tr>
-		<tr>
-			<th align="left" style="background: #d4d6d8" >Dirección</th>
+			<th align="left" style="background: #d4d6d8" >ALUMNO</th>
 			<td align="left"><?= isset( $model->alumno)?$model->alumno->nombre : '?' ;?></td>
-			<th align="right" style="background: #d4d6d8" >Fecha elaboración</th>
-			<td align="right">$model->fecha_venta;?></td>
+			
+			
 	
 						
 		</tr>
-		<tr>
-			<th align="left" style="background: #d4d6d8" >Telefono</th>
-			<td align="left">isset( $model->claveProveedor)?$model->claveProveedor->telefono : 'Sin telefono' ;?></td>
-			<th align="right" style="background: #d4d6d8" >Modo pago</th>
-			<td align="right">Contado</td>
+			<tr>
+			
+			<th align="left" style="background: #d4d6d8;"  >TALLER </th>
+			<td align="left" ><?=$model->tallerImp->nombre;?></td>
+			
+								
+		</tr>
+		
+			<tr>
+			
+			<th align="left" style="background: #d4d6d8;"  >INSTRUCTOR</th>
+			<td align="left" ><?=$model->tallerImp->instructor->nombre;?></td>
+			
+								
 		</tr>
 
 	
@@ -72,8 +105,8 @@
 			<th align="left" style="background: #d4d6d8;" >Item</th>
 			<th align="left" style="background: #d4d6d8;">Numero serie</th>
 			<th align="left" style="background: #d4d6d8;">Codigo desbloqueo</th>				
-			<th align="left" style="background: #d4d6d8;">Nombre</td>
-			<th align="left" style="background: #d4d6d8;">Precio unitario</td>
+			<th align="left" style="background: #d4d6d8;">Nombre</th>
+			<th align="left" style="background: #d4d6d8;">Precio unitario</th>
 		</tr>
 		<tbody>
 			
