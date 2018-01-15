68ae7e9c1702e606caa75f7443a87e77abb5df08
rafa <rafa@192.168.1.67>
2018-01-15 00:44:36 -0600

    pdf clase

--- backend/controllers/AlumnoController.php
+++ backend/controllers/AlumnoController.php
@@ -9,6 +9,7 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use mPDF;
+use kartik\mpdf\Pdf;
 
 /**
  * AlumnoController implements the CRUD actions for Alumno model.
@@ -116,6 +117,57 @@ class AlumnoController extends Controller
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
+    }
+    
+    /**
+     * Prints new  instription
+     * @param unknown $id
+     */
+    public function actionImprimirComprobante($id){
+        
+        //	Yii::$app->response->format = 'pdf';
+        
+        $model = Alumno::findOne($id);
+        
+        
+        if ($model  === null) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+        
+        $content = $this->renderPartial('ficha-alumno',['model'=>$model]);
+        
+        
+        $pdf = new Pdf([
+            // set to use core fonts only
+            'mode' => Pdf::MODE_UTF8,
+            // A4 paper format
+            'format' => Pdf::FORMAT_A4,
+            // portrait orientation
+            'orientation' => Pdf::ORIENT_PORTRAIT,
+            // stream to browser inline
+            //'destination' => Pdf::DEST_BROWSER,
+            // your html content input
+            'content' => $content,
+            // format content from your own css file if needed or use the
+            // enhanced bootstrap css built by Krajee for mPDF formatting
+            'cssFile' => '@vendor/kartik-v/yii2-mpdf/assets/kv-mpdf-bootstrap.min.css',
+            // any css to be embedded if required
+            'cssInline' => '.kv-heading-1{font-size:18px}
+    							#menu{
+								      font:5px;
+								    }',
+            // set mPDF properties on the fly
+            'options' => ['title' => 'Ficha de inscripción'],
+            // call mPDF methods on the fly
+            'methods' => [
+                'SetHeader'=>['Ficha de inscripción'],
+                'SetFooter'=>['{PAGENO}'],
+            ]
+        ]);
+        
+        // return the pdf output as per the destination setting
+        return $pdf->render();
+        
     }
 
     /**
