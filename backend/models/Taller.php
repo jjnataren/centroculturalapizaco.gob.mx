b5911a3e4c1b7f5285b3a1951ab61d09fcf5cfe7
rafa <rafa@192.168.1.71>
2018-02-07 23:14:54 -0600

    taller test

--- backend/models/Taller.php
+++ backend/models/Taller.php
@@ -3,13 +3,14 @@
 namespace backend\models;
 
 use Yii;
+use trntv\filekit\behaviors\UploadBehavior;
 
 /**
  * This is the model class for table "tbl_taller".
  *
- * @property integer $id
- * @property integer $id_instructor
- * @property integer $id_aula
+ * @property int $id
+ * @property int $id_instructor
+ * @property int $id_aula
  * @property string $nombre
  * @property string $descripcion
  * @property string $descripcion_temario
@@ -17,26 +18,52 @@ use Yii;
  * @property string $imagen_url
  * @property string $temario_url
  * @property string $fecha_creacion
- * @property integer $creado_por
- * @property integer $duracion_mes
- * @property integer $duracion_hora
- * @property integer $lunes
- * @property integer $martes
- * @property integer $miercoles
- * @property integer $jueves
- * @property integer $viernes
- * @property integer $sabado
- * @property integer $domingo
+ * @property int $creado_por
+ * @property int $duracion_mes
+ * @property int $duracion_hora
+ * @property int $lunes
+ * @property int $martes
+ * @property int $miercoles
+ * @property int $jueves
+ * @property int $viernes
+ * @property int $sabado
+ * @property int $domingo
  * @property string $hora_inicio
- * @property integer $disponible
+ * @property int $disponible
+ * @property int $id_categoria
+ * @property string $base_url
+ * @property string $path
  *
  * @property CuotaTaller[] $cuotaTallers
- * @property Aula $idAula
- * @property Instructor $idInstructor
+ * @property Aula $aula
+ * @property Categoria $categoria
+ * @property Instructor $instructor
  * @property TallerImp[] $tallerImps
  */
 class Taller extends \yii\db\ActiveRecord
 {
+    
+    /**
+     * @var
+     */
+    public $imagen_url;
+    
+    
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            'imagen_url' => [
+                'class' => UploadBehavior::className(),
+                'attribute' => 'imagen_url',
+                'pathAttribute' => 'path',
+                'baseUrlAttribute' => 'base'
+            ]
+        ];
+    }
+    
     /**
      * @inheritdoc
      */
@@ -51,11 +78,12 @@ class Taller extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id_instructor', 'id_aula', 'creado_por', 'duracion_mes', 'duracion_hora', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo', 'disponible'], 'integer'],
-            [['fecha_creacion', 'hora_inicio'], 'safe'],
+            [['id_instructor', 'id_aula', 'creado_por', 'duracion_mes', 'duracion_hora', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo', 'disponible', 'id_categoria'], 'integer'],
+            [['fecha_creacion', 'hora_inicio','imagen_url'], 'safe'],
             [['nombre', 'descripcion', 'descripcion_temario', 'numero_personas'], 'string', 'max' => 45],
-            [['imagen_url', 'temario_url'], 'string', 'max' => 300],
+            [[ 'temario_url', 'base_url', 'path'], 'string', 'max' => 300],
             [['id_aula'], 'exist', 'skipOnError' => true, 'targetClass' => Aula::className(), 'targetAttribute' => ['id_aula' => 'id']],
+            [['id_categoria'], 'exist', 'skipOnError' => true, 'targetClass' => Categoria::className(), 'targetAttribute' => ['id_categoria' => 'id']],
             [['id_instructor'], 'exist', 'skipOnError' => true, 'targetClass' => Instructor::className(), 'targetAttribute' => ['id_instructor' => 'id']],
         ];
     }
@@ -88,6 +116,9 @@ class Taller extends \yii\db\ActiveRecord
             'domingo' => 'Domingo',
             'hora_inicio' => 'Hora Inicio',
             'disponible' => 'Disponible',
+            'id_categoria' => 'Id Categoria',
+            'base_url' => 'Base Url',
+            'path' => 'Path',
         ];
     }
 
@@ -102,7 +133,7 @@ class Taller extends \yii\db\ActiveRecord
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getIdAula()
+    public function getAula()
     {
         return $this->hasOne(Aula::className(), ['id' => 'id_aula']);
     }
@@ -110,7 +141,15 @@ class Taller extends \yii\db\ActiveRecord
     /**
      * @return \yii\db\ActiveQuery
      */
-    public function getIdInstructor()
+    public function getCategoria()
+    {
+        return $this->hasOne(Categoria::className(), ['id' => 'id_categoria']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getInstructor()
     {
         return $this->hasOne(Instructor::className(), ['id' => 'id_instructor']);
     }
