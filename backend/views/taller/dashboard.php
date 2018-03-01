0eeeacf24af896465fed2eb1d56589bb65094c9a
jesus <jesus.nataren@gmail.com>
2018-02-28 20:39:34 -0600

    Cambios para el esttus del taller

--- backend/views/taller/dashboard.php
+++ backend/views/taller/dashboard.php
@@ -6,6 +6,7 @@ use yii\bootstrap\ActiveForm;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use backend\models\CuotaTaller;
+use backend\models\constants\Constantes;
 
 
 
@@ -21,6 +22,25 @@ $cuotaList=ArrayHelper::map
 (Cuota::findBySql('select id,  CONCAT(id, \' - \',concepto ) as concepto from tbl_cuota where disponible = 1')->all(), 'id', 'concepto');
 
 ?>
+<div class="row">
+
+ <div >
+   	<?php 
+   	
+   	    $cuotaDBTest = Cuota::find()->all();
+   	
+   	    foreach ($cuotaDBTest as $itemCuota){
+            
+            echo Html::input('hidden','cuota_monto'.$itemCuota->id, $itemCuota->monto, ['id'=>'cuota_monto'.$itemCuota->id]);
+            echo Html::input('hidden','cuota_concepto'.$itemCuota->id, $itemCuota->concepto, ['id'=>'cuota_concepto'.$itemCuota->id]);
+            
+        }   	    
+   	?>
+   		
+   		
+   	
+   </div>
+
 <div class="col-md-12 col-sm-12 col-xs-12">
 
 
@@ -158,12 +178,12 @@ $cuotaList=ArrayHelper::map
              	        'nombre',
              	        [
              	            'attribute'=>'id_cuota',
-             	            'header'=>'Concepto',
+             	            'header'=>'Tipo de cuota',
              	            'content'=>function($data){
              	            
              	            //   $categoriaproducto = isset(TipoProducto::$categoriaDesc[$data->tipoProducto->categoria]) ? TipoProducto::$categoriaDesc[$data->tipoProducto->categoria] : 'Desconocido';
              	        
-             	        return  $data->idCuota->concepto;
+             	        return   isset( $data->idCuota->concepto)?$data->idCuota->concepto:'?';
              	        
              	            },
              	            'filter'=>ArrayHelper::map(Cuota::findAll([ 'disponible'=>1]), 'id','concepto'),
@@ -255,7 +275,7 @@ $cuotaList=ArrayHelper::map
         <div class="col-md-12">
                <div class="box box-primary with-border">
             <div class="box-header with-border">
-              <h3 class="box-title">Talleres disponibles.</h3>
+              <h3 class="box-title">Historial de Talleres impartidos o por impartir.</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -284,21 +304,57 @@ $gridColumns = [
     [
         'attribute' => 'fecha_inicio',
         'vAlign'=>'middle',
+        'content'=>function($data){
+        
+        return  Yii::$app->formatter->asDate($data->fecha_inicio,'dd/MMM/Y');
+        },
         
     ],
     [
         'attribute' => 'fecha_fin',
         'vAlign'=>'middle',
+        'content'=>function($data){
+        
+        return  Yii::$app->formatter->asDate($data->fecha_fin,'dd/MMM/Y');
+        },
     ],
-    'estatus',
+    
+    [
+        'attribute' => 'estatus',
+        'format' => 'raw',
+        'value'=>function($data){
+        
+            $currentDate  = date('Y-m-d');
+            
+            $inicion = $data->fecha_inicio;
+            
+            if (isset($data->fecha_inicio) &&  $currentDate < $data->fecha_inicio ){
+                return '<p class="label label-info"><i class="fa fa-clock-o"></i>  '.Constantes::getTallerEstatusDesc(Constantes::TALLER_ESTATUS_POR_IMPARTIR).' </p>';
+            }
+           
+            if (isset($data->fecha_fin) &&  $currentDate > $data->fecha_fin ){
+                return '<p class="label label-info"><i class="fa fa-clock-o"></i>  '.Constantes::getTallerEstatusDesc(Constantes::TALLER_ESTATUS_FINALIZADO).' </p>';
+            }
+            
+            
+            if( (isset($data->fecha_inicio) &&  $currentDate >= $data->fecha_inicio )&& (isset($data->fecha_fin) &&  $currentDate <= $data->fecha_fin ) ) {
+                return '<p class="label label-warning"><i class="fa fa-clock-o"></i>  '.Constantes::getTallerEstatusDesc(Constantes::TALLER_ESTATUS_IMPARTIENDO).' </p>';
+            }
+            
+            return '<p class="label label-default"><i class="fa fa-plus"></i>  '.Constantes::getTallerEstatusDesc(-1).' </p>';
+        
+        },
+    ],
+    
+    
     ['class' => 'yii\grid\ActionColumn',
         'template' => '{view}',
         'buttons' => [
             
             'view' => function ($url, $model, $key) {
             //Html::a('borrar', ['cuota-taller/delete','id'=>$key], ['class' => 'bg-red label']);
-    return Html::a('', ['taller-imp/dashboard', 'id'=>$model->id], [
-    'class' => 'fa fa-tachometer','data-pjax' => '0',
+    return Html::a('<i class="fa fa-tachometer"></i> Admin', ['taller-imp/dashboard', 'id'=>$model->id], [
+    'class' => 'btn btn-primary','data-pjax' => '0',
     ]);
             }
             ]
@@ -348,7 +404,7 @@ echo GridView::widget([
 
 
 
-
+</div>
 
 	<div class="modal fade" id="modal-default">
           <div class="modal-dialog">
@@ -374,7 +430,9 @@ echo GridView::widget([
 
 
  <?= $form->field($cuotaModel, 'id_cuota',['template' => 
-		     		'<div class="form-group">
+		     		'
+                    {label} 
+                    <div class="form-group">
 		       		 <div class="input-group">
 		          <span class="input-group-addon" >
 		             <span class="fa fa-sticky-note"></span>
@@ -386,19 +444,36 @@ echo GridView::widget([
 		      <div> {error}{hint}</div>
    				</div>'])->dropDownList($cuotaList,
    						['prompt'=>'-- TIPO DE CUOTA  --',
-   						'id' => 'selectPro',
-   						'onchange'=>'
-			                $.get( "'.Yii::$app->urlManager->createUrl('tipo-producto/get-img?id=').'"+$(this).val(), function( data ) {
-			                  $( "#divimg" ).html( data );
-			                });
-            			',
+   						'id' => 'selectProCuota',
+   						'onchange'=>'var id_cuota = $("#selectProCuota option:selected").val();
+                                    $("#cuota_monto").val($("#cuota_monto"+id_cuota).val());
+                                    $("#cuota_concepto").val($("#cuota_concepto"+id_cuota).val());'
+   						    
+   						,
    						
-      ]) ?>
+      ])->label('Tipo de cuota'); ?>
+      
+      <?php echo $form->field($cuotaModel, 'nombre', ['template' => 
+		     		'
+                    {label} 
+                    <div class="form-group">
+		       		 <div class="input-group">
+		          <span class="input-group-addon" >
+		             <span class="fa fa-pencil"></span>
+		          </span>
+		          {input}
+		     		
+		       </div>
+		     			
+		      <div> {error}{hint}</div>
+   				</div>'])->textInput(['id'=>'cuota_concepto',  'placeholder' => 'Alias para esta cuota','class'=>'form-control input-lg','maxlength' => '16'])->label(false); ?>
+      
+      
       
       <?php echo $form->field($cuotaModel, 'obligatoria')->checkbox(['class'=>'form']); ?>
       
        <?= $form->field($cuotaModel, 'tipo_periodo',['template' => 
-		     		'<div class="form-group">
+		     		' <div class="form-group">
 		       		 <div class="input-group">
 		          <span class="input-group-addon" >
 		             <span class="fa fa-calendar-o"></span>
@@ -410,30 +485,12 @@ echo GridView::widget([
 		      <div> {error}{hint}</div>
    				</div>'])->dropDownList([2=>'Semanal', 1=>'Mensual', 3=>'Anual'],
    						['prompt'=>'-- PERIODICIDAD  --',
-   						'id' => 'selectPro',
-   						'onchange'=>'
-			                $.get( "'.Yii::$app->urlManager->createUrl('tipo-producto/get-img?id=').'"+$(this).val(), function( data ) {
-			                  $( "#divimg" ).html( data );
-			                });
-            			',
+   						'id' => 'selectPeriodo',
    						
       ]) ?>
       
       
-       <?php echo $form->field($cuotaModel, 'nombre', ['template' => 
-		     		'<div class="form-group">
-		       		 <div class="input-group">
-		          <span class="input-group-addon" >
-		             <span class="fa fa-pencil"></span>
-		          </span>
-		          {input}
-		     		
-		       </div>
-		     			
-		      <div> {error}{hint}</div>
-   				</div>'])->textInput(['placeholder' => 'Alias para esta cuota','class'=>'form-control input-lg','maxlength' => '16'])->label(false); ?>
-      
-      
+       
       
         <?php echo $form->field($cuotaModel, 'comentario', ['template' => 
 		     		'<div class="form-group">
--- backend/views/taller/index.php
+++ backend/views/taller/index.php
@@ -1,22 +1,78 @@
 <?php
 
+use backend\models\Categoria;
+use kartik\grid\GridView;
 use yii\helpers\Html;
-use yii\grid\GridView;
-
-/* @var $this yii\web\View */
-/* @var $searchModel backend\models\search\TallerSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
-$this->title = 'Talleres';
+$this->title = 'Talleres disponibles';
 $this->params['breadcrumbs'][] = $this->title;
+
+$categorias  = Categoria::findAll(['disponible'=>1]);
+
 ?>
-<div class="taller-index">
+<div class="row">
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+<div class="col-md-12">
+<div class="callout callout-info">
+                <h4>Talleres base</h4>
+                <p>En esta sección podras encontrar los talleres base  que imparte el contro cultural.</p>
+</div>
+</div>
 
-    <p>
-        <?php echo Html::a('Crear Taller', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+<?php foreach ($categorias as $categoria):?>
+
+<div class="col-md-12">
+	<div class="box box-info with-border">
+        <div class="box-header with-border">
+        	<i class="fa fa-sitemap"></i>
+          <h2 class="box-title"><?= $categoria->nombre ?></h2>
+    
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+            
+         </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+        <?php foreach ($categoria->tallers as $taller):?>
+        <div class="col-md-4">
+            <div class="thumbnail">
+              <img src="<?php echo $taller->base_url . '/' .$taller->path; ?>" alt="...">
+              <div class="caption">
+                <h3><?php echo $taller->nombre;?></h3>
+                <p><?php echo $taller->descripcion;?></p>
+                <p><strong>Instructor:</strong>&nbsp; <?php echo isset( $taller->instructor->nombre)?$taller->instructor->nombre: '?'; ?></p>
+                <p><a href="/taller/dashboard?id=<?=$taller->id?>" class="btn btn-primary" role="button">Administrar</a> <a href="/taller/update?id=<?=$taller->id?>" class="btn btn-warning" role="button">Editar</a></p>
+              </div>
+            </div>
+    	</div>
+    	<?php endforeach;?>
+	</div>
+</div>
+		
+</div>			
+<?php endforeach;?>
+
+
+<div class="col-md-12">
+	<div class="box box-info with-border">
+        <div class="box-header with-border">
+        	<i class="fa fa-sitemap"></i>
+          <h2 class="box-title">Todos</h2>
+    
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+            
+         </div>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+
+
+
 
     <?php echo GridView::widget([
         'dataProvider' => $dataProvider,
@@ -25,7 +81,15 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
-            'id_instructor',
+            [
+                
+             'header'=>'Instructor',
+             'attribute'=>'id_instructor',
+             'value'=>function($data){                 
+             return isset( $data->instructor->nombre)?$data->instructor->nombre: '?' ;
+             }
+            ]
+            ,
             'nombre',
             'descripcion',
             'descripcion_temario',
@@ -39,6 +103,35 @@ $this->params['breadcrumbs'][] = $this->title;
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
+        'resizableColumns'=>true,
+        
+        //    'containerOptions' => ['style'=>'overflow: auto'], // only set when $responsive = false
+            
+            'toolbar' =>  [
+                ['content'=>
+                    Html::a('<i class="glyphicon glyphicon-plus"></i>', ['create'], ['data-pjax'=>0, 'class' => 'btn btn-primary', 'title'=>'Agregar nuevo']) . ' '.
+                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', ['index'], ['data-pjax'=>0, 'class' => 'btn btn-default', 'title'=>Yii::t('kvgrid', 'Reset Grid')])
+                ],
+                '{export}',
+                '{toggleData}'
+            ],
+            'pjax' => true,
+            'bordered' => true,
+            'striped' => true,
+            'condensed' => true,
+            'responsive' => true,
+            'hover' => true,
+            'floatHeader' => true,
+            'floatHeaderOptions' => ['scrollingTop' => false],
+            'panel' => [
+                'type' => GridView::TYPE_PRIMARY
+            ],
+        
     ]); ?>
+    
+    </div>
+    </div>
+    </div>
+    
 
 </div>
