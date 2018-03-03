667b2cbb3dbd218f07bc2494c66e2cd027b15ff6
rafa <rafa@192.168.1.74>
2018-03-03 02:17:42 -0600

    MODIFICACION DE FICHA

--- backend/views/alumno/ficha-alumno.php
+++ backend/views/alumno/ficha-alumno.php
@@ -14,7 +14,7 @@
     		<th width="300" align="left"><img alt="" src="/img/gobtlax.jpg"  height="130"></th>
     		<th align="center" valign="bottom" colspan="2" >"LA LIBERTAD"</th>
     		
-    		<th width="300"align="right"><img alt="" src="/img/logoLCC.jpg" width="300" height="100"></th>
+    		<th width="300"align="right"><img alt="" src="/img/logoLCC.jpg"  height="100"></th>
 		</tr>
 			
 </table>
@@ -38,237 +38,145 @@
 
 </table>
 
-<br />
 
-<table  style="width: 100%; font-size: 12px;  font-family:times new roman; font-style:bold;">
-			
-		<tr >
-			<td style=" width: 20%;"></td>	
-			<td style=" width: 40%;"></td>
-			<td style=" width: 20%;"></td>
-			<td style=" width: 20%;"></td>
-		</tr>	
-		
-		
-			
-		
-</table>	
+
+
 <table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
 	<tr>
-		<td width= "100%" ><b>NOMBRE:</b> <u>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-	
-	
-	&nbsp;&nbsp;<?= $model->nombre; ?>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-	
-	&nbsp;&nbsp;</u></></td>
+		<td width="10%"><b>NOMBRE:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->nombre; ?></td>
 	</tr>
+</table>
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
 	<tr>
-		<td><B>TALLER AL QUE SE INCRIBE EL ALUMNO(A):</B> <u>&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	
-
-	&nbsp;&nbsp;<?= $model->taller_inscribe; ?>&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-		&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	</u></td>
+		<td width="38%"><b>TALLER AL QUE SE INCRIBE EL ALUMNO(A):</b></td>
+		<td style="border-bottom: 1px solid #000000"></td>
 	</tr>
-	<tr>
-		<td><b>FECHA DE INGRESO:</b> <u>&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-	&nbsp;&nbsp;<?= $model->fecha_ingreso; ?>&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;</u></td>
-	</tr>
-	<tr>
-	<td><b>FECHA DE NACIMIENTO:</b> <u>&nbsp;&nbsp;&nbsp;<?= $model->fecha_nacimiento; ?>	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp; &nbsp;
-	&nbsp;&nbsp;</u> <b>LUGAR DE NACIMIENTO:</b>  <u>&nbsp;&nbsp;&nbsp;&nbsp;<?= $model->lugar_nacimiento; ?> &nbsp;
-	&nbsp;</u></td>
-	</tr>
-	<tr>
-		<td><b>DOMICILIO:</b> <u>&nbsp;
-	&nbsp;<?= $model->direccion;?>
-	</u>&nbsp;&nbsp;<b>LOCALIDAD:</b> <u>&nbsp;
-	&nbsp;<?= $model->nacionalidad;?>
-	</u>&nbsp;&nbsp;<b>TELEFONO:</b> <u>&nbsp;&nbsp;&nbsp;
-	<?= $model->telefono_casa;?>&nbsp;&nbsp;&nbsp;
-	
-	</u></td>
-	</tr>
-	<tr>
-	<td><b>NOMBRE DEL PADRE:</b> <u>&nbsp;&nbsp;&nbsp;<?= $model->nombre_padre; ?> &nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	</u> <b>EDAD:</b>  <u>&nbsp;&nbsp;&nbsp;&nbsp;<?= $model->edad_padre; ?> &nbsp; años 
-	&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;</u></td>
-	</tr>
-	
-	<tr>
-	<td><b>OCUPACION:</b> <u>&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;<?= $model->ocupacion_padre; ?> &nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	
-	</u> <b>TELEFONO:</b>  <u>&nbsp;&nbsp;&nbsp;&nbsp;<?= $model->tel_padre; ?> &nbsp; 
-	&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;</u></td>
-	</tr>
-	
-	
-	
-	<tr>
-	<td><b>NOMBRE DEL LA MADRE: </b><u>&nbsp;&nbsp;&nbsp;<?= $model->nombre_madre; ?> &nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	</u> <b>EDAD: </b> <u>&nbsp;&nbsp;&nbsp;&nbsp;<?= $model->edad_madre; ?> &nbsp; años 
-	&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;</u></td>
-	</tr>
-	
-	<tr>
-	<td><b>OCUPACION: <u>&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;<?= $model->ocupacion_madre; ?> &nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	
-	</u> <b>TELEFONO: </b> <u>&nbsp;&nbsp;&nbsp;&nbsp;<?= $model->tel_madre; ?> &nbsp; 
-	&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;</u></td>
-	</tr>
-	
-		<tr>
-		<td width= "100%" ><b>EN CASO DE EMERGENCIA LLAMAR AL:</b> <u>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-	
-	
-	&nbsp;&nbsp;<?= $model->tel_emergencia; ?>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-	
-	&nbsp;&nbsp;</u></></td>
-	</tr>
-	
-		<tr>
-		<td width= "100%" ><b>NOMBRE DE LA ESCUELA DONDE ESTUDIA ACTUALMENTE: </b><u>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-	
-	
-	&nbsp;&nbsp;<?= $model->escuela_procedencia; ?>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;
-
-
-
-	
-	&nbsp;&nbsp;</u></></td>
-	</tr>
-	
-		<tr>
-		<td width= "100%" ><b>ALERGIA O ENFERMEDAD CRÓNICA: </b><u>&nbsp;
-	&nbsp;&nbsp;&nbsp;	
-	&nbsp;&nbsp;<?= $model->alergia_enfermedad; ?>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;	
-	&nbsp;&nbsp;</u></></td>
-	</tr>
-	</table>
+</table>
 	<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
-		<tr>
-		<td width= "100%" ><b>TIPO SANGUINEO: </b><u>&nbsp;
-	&nbsp;&nbsp;&nbsp;	
-	&nbsp;&nbsp;<?= $model->tipo_sangineo; ?>&nbsp;
-	&nbsp;&nbsp;&nbsp;
-	&nbsp;&nbsp;&nbsp;	
-	&nbsp;&nbsp;</u><b>ESTA AFILIADO A:</b>	&nbsp;&nbsp;&nbsp;<b>IMSS</b>	_______	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	<b>ISSSTE</b> _______	&nbsp;&nbsp;&nbsp;	&nbsp;&nbsp;&nbsp;	<b>SEGURO POPULAR</b> _______	</td>
+	<tr>
+		<td width="17%"><b>FECHA DE INGRESO:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->fecha_ingreso; ?></td>
 	</tr>
+</table>
+
+
+
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="20%"><b>FECHA DE NACIMIENTO:</b></td>
+		<td width="23%" style="border-bottom: 1px solid #000000"><?= $model->fecha_nacimiento; ?></td>
+		
+		<td width="20%"><b>LUGAR DE NACIMIENTO:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->lugar_nacimiento; ?></td>
+	</tr>
+</table>
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="10%"><b>DOMICILIO:</b></td>
+		<td width="40%" style="border-bottom: 1px solid #000000"><?= $model->direccion; ?></td>
+		
+		<td width="11%"><b>LOCALIDAD:</b></td>
+		<td width="20%"  style="border-bottom: 1px solid #000000"><?= $model->nacionalidad; ?></td>
+		<td width="10%"><b>TELEFONO:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->telefono_casa; ?></td>
+	</tr>
+</table>
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="20%"><b>NOMBRE DEL PADRE:</b></td>
+		<td width="60%" style="border-bottom: 1px solid #000000"><?= $model->nombre_padre; ?></td>
+		
+		<td width="7%"><b>EDAD:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->edad_padre; ?></td>
+	</tr>
+</table>
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="12%"><b>OCUPACION:</b></td>
+		<td width="50%" style="border-bottom: 1px solid #000000"><?= $model->ocupacion_padre; ?></td>
+		
+		<td width="10%"><b>TELEFONO:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->tel_padre; ?></td>
+	</tr>
+</table>
+
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="20%"><b>NOMBRE DE LA MADRE:</b></td>
+		<td width="60%" style="border-bottom: 1px solid #000000"><?= $model->nombre_madre; ?></td>
+		
+		<td width="7%"><b>EDAD:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->edad_madre; ?></td>
+	</tr>
+</table>
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="12%"><b>OCUPACION:</b></td>
+		<td width="50%" style="border-bottom: 1px solid #000000"><?= $model->ocupacion_madre; ?></td>
+		
+		<td width="10%"><b>TELEFONO:</b></td>
+		<td style="border-bottom: 1px solid #000000"><?= $model->tel_madre; ?></td>
+	</tr>
+</table>
+
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="32%"><b>EN CASO DE EMERGENCIA LLAMAR AL:</b></td>
+		<td  style="border-bottom: 1px solid #000000"><?= $model->tel_emergencia; ?></td>
+		
+		
+	</tr>
+</table>
+
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="48%"><b>NOMBRE DE LA ESCUELA DONDE ESTUDIA ACTUALMENTE:</b></td>
+		<td  style="border-bottom: 1px solid #000000"><?= $model->escuela_procedencia; ?></td>
+		
+		
+	</tr>
+</table>
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="29%"><b>ALERGIA O ENFERMEDAD CRÓNICA:</b></td>
+		<td  style="border-bottom: 1px solid #000000"><?= $model->alergia_enfermedad; ?></td>
+		
+		
+	</tr>
+</table>
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="10%"><b> TIPO SANGINEO:</b></td>
+		<td   width="36%" style="border-bottom: 1px solid #000000"><?= $model->alergia_enfermedad; ?></td>
+		
+		
+		
+		
+		
+	</tr>
+</table>
+<table border="" style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
+	<tr>
+		<td width="14%"><b>TIPO SANGINEO:</b></td>
+		<td width="30%" style="border-bottom: 1px solid #000000"><?= $model->alergia_enfermedad; ?></td>
+		
+		<td width="20%"><b>ESTA AFILIADO A:  IMSS</b></td>
+		<td width="4%" style="border-bottom: 1px solid #000000"></td>
+		<td width="7%"><b>  ISSSTE</b></td>
+		<td width="4%" style="border-bottom: 1px solid #000000"></td>
+		<td width="15%"><b>  SEGURO POPULAR</b></td>
+		<td style="border-bottom: 1px solid #000000"></td>
+	</tr>
+</table>
 
 	
-</table>	
 <br><br><br><br>
 <table  style="width: 100%; font-size: 15px;  font-family:times new roman; font-style:bold;">
 	<tr>
